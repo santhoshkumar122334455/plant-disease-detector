0a3dd08f98407fd024dcf8e6b628024ecb2e5355
Santhosh Kumar <santoshkumar83gb@gmail.com>
2025-06-28 21:47:37 -0700

    🗑️ Remove Dockerfile and download_model.py

--- download_model.py
+++
@@ -1,5 +0,0 @@
-import gdown
-
-url = 'https://drive.google.com/file/d/1e0xmtz08OXyHWf5fjQbJSgxE_ABfE_G6/view?usp=sharing'
-output = 'efficientnet_checkpoint.keras'
-gdown.download(url, output, quiet=False)
